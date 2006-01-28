d870acc2bcb84a7a8fb5602be5f8cb4f8620c237
Christian Fowler <spider@viovio.com>
2006-01-28 03:05:05 +0000

    lucene is now working with tiki_content - simply but does a nice job

--- index.php
+++ index.php
@@ -3,29 +3,29 @@
 require_once( '../bit_setup_inc.php' );
 
 /* $field is the default field to be searched. */
-$field = "contents";
+$field = "data";
+
+$indexDir = $gBitSystem->getPreference( 'lucene_index_dir', TEMP_PKG_PATH.'lucene/index' );
+
+mkdir_p( $indexDir );
+
+$feedback = array();
 
 /* Creation of an IndexSearcher instance */
 try {
-	$searcher = new IndexSearcher( CLUCENE_PKG_PATH."data/index", $field);
+	$searcher = new IndexSearcher( $indexDir, $field );
 } catch (Exception $e) {
-	echo 'Message: ' . $e->getMessage() . ', ';
-	echo 'File: '    . $e->getFile()    . ', ';
-	echo 'Line: '    . $e->getLine();
-	exit();
+	$feedback['error'] = 'The search index is unavailable: '.$e->getMessage();
 }
 
-$query = '';
-if (isset($_SERVER['argv'][1])) {
-	$query = $_SERVER['argv'][1];
-} else {
-	$query = "time"; /* Should return 3 results */
+if( isset( $_REQUEST['search_phrase'] ) ) {
+	$query = $_REQUEST['search_phrase'];
+	$hits = $searcher->search( $query );
+	$gBitSmarty->assign_by_ref( 'searchHits', $hits->length() );
+	$gBitSmarty->assign_by_ref( 'searchResults', $hits );
 }
 
-$hits = $searcher->search( $query );
-$gBitSmarty->assign_by_ref( 'searchHits', $hits->length() );
-$gBitSmarty->assign_by_ref( 'searchResults', $hits );
-
+$gBitSmarty->assign_by_ref( 'feedback', $feedback );
 
 $gBitSystem->display( 'bitpackage:clucene/clucene_search.tpl' );
 
--- templates/clucene_search.tpl
+++ templates/clucene_search.tpl
@@ -6,13 +6,22 @@
 
 	<div class="body">
 
+	{formfeedback hash=$feedback}
+
+	{form}
+		<input type="text" name="search_phrase" value="{$smarty.request.search_phrase}" />
+		<input type="submit" name="{tr}Search{/tr}" />
+	{/form}
+
 {if $searchResults}
 <div>Number of results: {$searchHits}</div>
-<ul>
+
+<div style="float:right">{tr}Score{/tr}</div>
+<ul class="data">
 {section loop=$searchHits name=ix}
-	<li> path: {$searchResults->get($smarty.section.ix.index,'path')}<br />
-		 id: {$searchResults->id($smarty.section.ix.index)}<br/>
-		 score: {$searchResults->score($smarty.section.ix.index)*100|round:1}<br/>
+	<li class="item"><div style="float:right"> {$searchResults->score($smarty.section.ix.index)*100|round:1}</div>
+		<a href="{$smarty.const.BIT_ROOT_URL}index.php?content_id={$searchResults->id($smarty.section.ix.index)}">{$searchResults->get($smarty.section.ix.index,'title')}</a><br/>
+		 {$searchResults->get($smarty.section.ix.index,'content_type_guid')}<br />
 	</li>
 {/section}
 </ul>
